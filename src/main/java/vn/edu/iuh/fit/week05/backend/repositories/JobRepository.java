46547ec1ced28821136960eda6c34f07b4de19db
GiaHuyIUH <tranhuyzaza@gmail.com>
2024-12-15 20:24:43 +0700

    modify repositories

--- src/main/java/vn/edu/iuh/fit/week05/backend/repositories/JobRepository.java
+++ src/main/java/vn/edu/iuh/fit/week05/backend/repositories/JobRepository.java
@@ -3,13 +3,35 @@ package vn.edu.iuh.fit.week05.backend.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.edu.iuh.fit.week05.backend.models.Job;
+import vn.edu.iuh.fit.week05.backend.models.Skill;
 
 import java.util.List;
+
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
 
     Page<Job> findByCompanyId(Long companyId, Pageable pageable);
+
     Job findByIdAndCompanyId(Long id, Long companyId);
+
+    @Query("""
+                SELECT DISTINCT j
+                FROM Job j
+                JOIN JobSkill js ON j.id = js.id.jobId
+                JOIN CandidateSkill cs ON js.id.skillId = cs.id.skillId
+                WHERE cs.id.canId = :candidateId
+            """)
+    Page<Job> findMatchingJobsByCandidateId(@Param("candidateId") Long candidateId, Pageable pageable);
+
+    @Query("""
+                SELECT j
+                FROM Job j
+                JOIN JobSkill js ON j.id = js.id.jobId
+                WHERE js.id.skillId = :skillId
+            """)
+    List<Job> getJobBySkill(@Param("skillId") Long skillId);
 }
--- src/main/java/vn/edu/iuh/fit/week05/backend/repositories/SkillRepository.java
+++ src/main/java/vn/edu/iuh/fit/week05/backend/repositories/SkillRepository.java
@@ -1,6 +1,8 @@
 package vn.edu.iuh.fit.week05.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.edu.iuh.fit.week05.backend.models.Skill;
 
@@ -8,4 +10,11 @@ import java.util.List;
 
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Long> {
+    @Query(value = "SELECT s.* FROM skill s " +
+            "WHERE s.skill_id NOT IN (" +
+            "    SELECT skill_id FROM candidate_skill WHERE can_id = :candidateId" +
+            ")", nativeQuery = true)
+    List<Skill> findSkillNotLearnByCandidate(@Param("candidateId") Long candidateId);
+
+
 }
